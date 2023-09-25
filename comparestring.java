d5bd5ba316ced6f37d528f672a916ff64f47047d
Himanshi-goyal12 <144993741+Himanshi-goyal12@users.noreply.github.com>
2023-09-25 23:47:58 +0530

    main

---
+++ comparestring.java
@@ -0,0 +1,7 @@
+public class comparestring {
+    public static void main(String[] args) {
+        String str= "GLA University";
+        String str1 = "College name";
+        System.out.println(str.compareTo(str1));
+    }
+}
