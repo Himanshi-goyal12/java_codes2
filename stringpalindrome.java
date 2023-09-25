0ac04b5c656b9752b55ac386c599c84d60e6f1db
Himanshi-goyal12 <144993741+Himanshi-goyal12@users.noreply.github.com>
2023-09-25 22:33:07 +0530

    main

---
+++ stringpalindrome.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class stringpalindrome {
+    public static void main(String[] args) {
+        System.out.println("enter the string");
+        Scanner sc = new Scanner(System.in);
+        String str=sc.next();
+        
+    }
+}
